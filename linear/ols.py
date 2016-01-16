e20467ffd87ca8650a8aafbece40d5c406da5031
WangZhi <WangZhiCS@gmail.com>
2016-01-16 15:49:56 +0800

    finish linear regress with LWLR

---
+++ linear/ols.py
@@ -0,0 +1,44 @@
+import numpy as np
+from pandas import DataFrame
+
+
+class OrdinaryLeastSquares:
+    def __init__(self):
+        # train data
+        self._train_frame = DataFrame({"feature": [], "label": []})
+        # train result
+        self._had_regress = False
+        self._feature_mat = None
+        self._feature_mat_t = None
+        self._label_mat = None
+
+    def set_train_frame(self, train_frame):
+        self._train_frame["feature"] = train_frame["feature"]
+        self._train_frame["label"] = train_frame["label"]
+
+    def add_train_features(self, train_features, train_labels):
+        for i in range(len(train_features)):
+            self._train_frame = \
+                self._train_frame.append({"feature": train_features[i], "label": train_labels[i]}, ignore_index=True)
+
+    def regress(self, feature, k):
+        if not self._had_regress:
+            self._feature_mat = np.mat(self._train_frame["feature"].tolist())
+            self._feature_mat_t = self._feature_mat.transpose()
+            self._label_mat = np.mat(self._train_frame["label"].tolist()).transpose()
+        m, n = np.shape(self._feature_mat)
+        weights_eye = np.eye(m)
+        feature_mat = np.mat(feature)
+        for i in range(m):
+            differ_mat = feature_mat - self._feature_mat[i, :]
+            # LWLR: Locally Weighted Linear Regression
+            weights_eye[i, i] = np.exp(differ_mat * differ_mat.transpose() / (-2.0 * np.square(k)))
+        xTx = self._feature_mat_t * weights_eye * self._feature_mat
+        xTx_det = np.linalg.det(xTx)
+        if xTx_det == 0.0:
+            print("the xTx matrix is singular, can not do inverse")
+            return
+        xTx_inv = np.linalg.inv(xTx)
+        weights = xTx_inv * self._feature_mat_t * weights_eye * self._label_mat
+        # get the value of 1 * 1 ,atrix
+        return np.sum(feature_mat * weights)
---
+++ test/linear/run.py
@@ -0,0 +1,54 @@
+from linear.ols import OrdinaryLeastSquares
+import matplotlib.pyplot as plt
+
+
+def read_data():
+    x_rows = []
+    y_rows = []
+    try:
+        with open("ex0.txt", "r") as file:
+            data_row = []
+            tmp_str = ''
+            for each_line in file:
+                line_array = each_line.strip().split("\t")
+                x_rows.append([float(line_array[0]), float(line_array[1])])
+                y_rows.append(float(line_array[2]))
+        return x_rows, y_rows
+    except IOError as io_error:
+        print(str(io_error))
+
+if __name__ == '__main__':
+    x_arrays, y_array = read_data()
+    ols = OrdinaryLeastSquares()
+    ols.add_train_features(x_arrays, y_array)
+    # sort x_arrays by x2
+    xy_array = []
+    for i in range(len(x_arrays)):
+        xy_array.append([x_arrays[i], y_array[i]])
+    xy_array.sort(key=lambda x: x[0][1])
+    # rebuild x_arrays, y_array
+    x_arrays = []
+    y_array = []
+    for xy in xy_array:
+        x_arrays.append(xy[0])
+        y_array.append(xy[1])
+    # regress
+    x_array = []
+    y1_array = []
+    y2_array = []
+    y1 = 0
+    y2 = 0
+    for i in range(len(x_arrays)):
+        y1 = ols.regress(x_arrays[i], 1.0)
+        y2 = ols.regress(x_arrays[i], 0.01)
+        x_array.append(x_arrays[i][1])
+        y1_array.append(y1)
+        y2_array.append(y2)
+    # paint
+    plt.ylim((0.0, 1.2))
+    plt.ylim((3.0, 5.2))
+    plt.scatter(x_array, y_array, alpha=0.5, s=10)
+    plt.plot(x_array, y1_array, label="K=1.0", color="green", linestyle="solid")
+    plt.plot(x_array, y2_array, label="K=0.01", color="black", linestyle="dashed")
+    plt.legend()
+    plt.savefig("ols.png")
