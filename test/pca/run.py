767ece6a40e6f612137665d731349774dbb6f141
WangZhi <WangZhiCS@gmail.com>
2016-01-27 13:24:38 +0800

    finish paint the new coordinate

--- test/pca/run.py
+++ test/pca/run.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pca.core import PCA
+import matplotlib.pyplot as plt
 
 
 def load_data(file_name):
@@ -15,5 +16,12 @@ if __name__ == '__main__':
     data = load_data("testSet.txt")
     data_mat = np.mat(data)
     pca = PCA(data_mat, 1)
-    print(pca.get_low_dimension())
-    print(pca.new_coordinate())
+    new_coordinate = pca.new_coordinate()
+    new_coordinate_mat = np.mat(new_coordinate)
+    old_x = data_mat[:, 0].tolist()
+    old_y = data_mat[:, 1].tolist()
+    new_x = new_coordinate_mat[:, 0].tolist()
+    new_y = new_coordinate_mat[:, 1].tolist()
+    plt.scatter(old_x, old_y, c="green", s=10, marker="^", alpha=0.8)
+    plt.scatter(new_x, new_y, c="blue", s=10, marker="o")
+    plt.savefig("pca.png")
